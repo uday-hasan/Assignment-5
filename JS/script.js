de265371bb6ede09445aff5f7833ea06303be0bc
uday-g-ithub15 <mudayhasan@gmail.com>
2022-02-17 12:29:46 +0600

    Some modification all file

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,23 @@
-console.log('Working');
+// function getInput(value) {
+//     const userInput = document.getElementById(value + 'input');
+// }
+
+document.getElementById('calculate-expenses').addEventListener('click', function () {
+    const userInput = document.getElementById('income-input');
+    const userIncome = parseFloat(userInput.value);
+    const foodInput = document.getElementById('food-input');
+    const foodExpenses = parseFloat(foodInput.value);
+    const rentInput = document.getElementById('rent-input');
+    const rentExpenses = parseFloat(rentInput.value);
+    const clothesInput = document.getElementById('clothes-input');
+    const clothesExpenses = parseFloat(clothesInput.value);
+    const expenses = foodExpenses + rentExpenses + clothesExpenses;
+
+    document.getElementById('total-expenses').innerText = expenses;
+    document.getElementById('balance').innerText = userIncome - expenses;
+    // document.getElementById('income-input').value = '';
+    userInput.value = '';
+    foodInput.value = '';
+    rentInput.value = '';
+    clothesInput.value = '';
+})
