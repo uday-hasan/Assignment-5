4070b6d5af520c9e5c2c1594d0d7786915ff4e08
uday-g-ithub15 <mudayhasan@gmail.com>
2022-02-17 18:17:42 +0600

    responsive and many js code

--- JS/script.js
+++ JS/script.js
@@ -1,36 +1,74 @@
 function getInput(inpt) {
-    const userInput = document.getElementById(inpt + 'input');
-    const userIncome = parseFloat(userInput);
-    return userIncome;
+    const userInput = document.getElementById(inpt + '-input');
+    const userValue = parseFloat(userInput.value);
+    if (userValue < 0) {
+        document.getElementById(inpt + '-error').style.display = 'block';
+        document.getElementById('display-expenses-balance').style.display = 'none';
+        return getInput();
+    }
+    else {
+        document.getElementById(inpt + '-error').style.display = 'none';
+        document.getElementById('display-expenses-balance').style.display = 'block'
+        return userValue;
+    }
 }
-const userInput = document.getElementById('income-input').addEventListener('focus', function () {
-    const he = getInput('income');
-    console.log(he);
-})
 
-/* document.getElementById('calculate-expenses').addEventListener('click', function () {
-    const userInput = document.getElementById('income-input');
-    const userIncome = parseFloat(userInput.value);
-    const foodInput = document.getElementById('food-input');
-    const foodExpenses = parseFloat(foodInput.value);
-    const rentInput = document.getElementById('rent-input');
-    const rentExpenses = parseFloat(rentInput.value);
-    const clothesInput = document.getElementById('clothes-input');
-    const clothesExpenses = parseFloat(clothesInput.value);
+function getExpenses() {
+    // Calling getInput() function to get user input
+    const userIncome = getInput('income');
+    const foodExpenses = getInput('food');
+    const rentExpenses = getInput('rent');
+    const clothesExpenses = getInput('clothes');
+
     const expenses = foodExpenses + rentExpenses + clothesExpenses;
-
     if (expenses > userIncome) {
         document.getElementById('display-expenses-balance').style.display = 'none';
         document.getElementById('expenses-error').style.display = 'block';
+        return getExpenses();
     }
-    else if (foodExpenses < 0) {
-        document.getElementById('food-error').style.display = 'block';
+    else {
+        document.getElementById('total-expenses').innerText = expenses;
+        const balance = userIncome - expenses;
+        document.getElementById('balance').innerText = balance;
+        document.getElementById('expenses-error').style.display = 'none';
     }
-    document.getElementById('total-expenses').innerText = expenses;
-    document.getElementById('balance').innerText = userIncome - expenses;
-    // document.getElementById('income-input').value = '';
-    userInput.value = '';
-    foodInput.value = '';
-    rentInput.value = '';
-    clothesInput.value = '';
-}) */
+    return (userIncome - expenses);
+}
+
+// Calculate Total Expenses And Balance
+document.getElementById('calculate-expenses').addEventListener('click', function () {
+    getExpenses();
+})
+
+
+document.getElementById('calculate-save').addEventListener('click', function () {
+    const userIncomeInput = document.getElementById('income-input');
+    const userIncome = parseFloat(userIncomeInput.value);
+    const userbalance = getExpenses();
+    const saveInput = parseFloat(document.getElementById('save').value);
+    const saveValue = (saveInput / 100);
+    // const saveValue = userIncome * (saveInput / 100);
+
+    const saveAmount = userIncome * saveValue;
+
+    if (saveAmount > userbalance) {
+        document.getElementById('saving-display').style.display = 'none';
+        document.getElementById('save-error').style.display = 'block';
+        document.getElementById('save-value-error').style.display = 'none';
+    }
+    else {
+        if (saveInput < 0) {
+            document.getElementById('save-value-error').style.display = 'block'
+            document.getElementById('save-error').style.display = 'none';
+            document.getElementById('saving-display').style.display = 'none';
+        }
+        else {
+            document.getElementById('saving-display').style.display = 'block';
+            document.getElementById('save-error').style.display = 'none';
+            document.getElementById('save-amount').innerText = saveAmount;
+            document.getElementById('remaining-amount').innerText = userbalance - saveAmount;
+        }
+    }
+})
+
+
